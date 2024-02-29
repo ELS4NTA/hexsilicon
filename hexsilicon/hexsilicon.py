4e3d079cd2f4c43e61a6e3aa3831da0d762e6ce6
Daniel Santanilla <daniel1sa_@hotmail.com>
2024-02-28 22:13:43 -0500

    Hexsilicon

---
+++ hexsilicon/hexsilicon.py
@@ -0,0 +1,18 @@
+from hexsilicon.swarm.population import ant
+from hexsilicon.problem import problem
+
+
+class Hexsilicon(object):
+
+    def __init__(self):
+        self.example_problem()
+        ant_swarm = ant.Ant()
+        ant_swarm.generate_initial_swarm()
+
+    def example_problem(self):
+        ej_problem = problem.Problem()
+
+
+
+if __name__ == "__main__":
+    hexsilicon = Hexsilicon()
