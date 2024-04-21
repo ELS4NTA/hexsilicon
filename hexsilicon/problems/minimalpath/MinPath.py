3df3e35ba8e67e5a9f2f2b1b32e2bd94b661747a
Angie Mojica <nataliamojica90@gmail.com>
2024-04-20 23:28:05 -0500

    Problem minimum

--- hexsilicon/problems/minimalpath/MinPath.py
+++ hexsilicon/problems/minimalpath/MinPath.py
@@ -1,16 +1,52 @@
+import pandas as pd
+import networkx as nx
+
+from hexsilicon.problems.functions.roadcost import RoadCostFunction
 from hexsilicon.problems.problem import Problem
 
 
 class MinPath(Problem):
 
-    def __init__(self):
+    def __init__(self, context):
         super().__init__()
+        self.function = RoadCostFunction()
+        self.context = context
+        self.representation = self.make_representation()
 
+    def get_next_nodes(self, current_node):
+        return list(self.representation.neighbors(current_node))
+
+    def check_restrictions(self, solution):
+        if solution[0] == self.df['initial'][0] and solution[-1] == self.df['final'][0]:
+            return True
+        return False
+        
+    def make_representation(self):
+        self.df = pd.read_csv(self.context, sep=";")
+        df_formatted = pd.DataFrame({
+            'source': self.df['source'],
+            'target': self.df['target'],
+            'weight': self.df['weight'].astype(float)
+        })
+        return nx.from_pandas_edgelist(df_formatted, 'source', 'target', ['weight'])
+    
+    def get_random_point(self):
+        return self.df['initial'][0]    
+        
     def get_representation(self):
-        pass
+        return self.representation
 
-    def get_description(self):
-        pass
+    def is_minimization(self):
+        return True if self.df['minimization'][0] == 'yes' else False
+
+    @staticmethod
+    def get_description():
+        return {
+            "name": "Camino Mínimo",
+            "description": "Consiste en determinar la distancia más corta que hay entre dos nodos de un grafo.",
+            "algorithms": "SACO, MMAS",
+            "class_name": "MinPath"
+        }
 
     def get_restrictions(self):
         pass
--- hexsilicon/problems/problem.py
+++ hexsilicon/problems/problem.py
@@ -3,20 +3,42 @@ from abc import abstractmethod
 
 class Problem(object):
 
-    def __init__(self):
+    def __init__(self, context=None):
         self.function = None
         self.searchspace = None
+        self.context = context
+    
+    @abstractmethod
+    def make_representation(self):
+        pass
 
     @abstractmethod
     def get_representation(self):
         pass
 
+    @staticmethod
     @abstractmethod
-    def get_description(self):
+    def get_description():
         pass
 
     @abstractmethod
     def get_restrictions(self):
+        pass
+    
+    @abstractmethod
+    def check_restrictions(self, solution):
+        pass
+    
+    @abstractmethod
+    def get_random_point(self):
+        pass
+    
+    @abstractmethod
+    def get_next_nodes(self, current_node):
+        pass
+    
+    @abstractmethod
+    def is_minimization(self):
         pass
 
     def call_function(self, solution) -> float:
